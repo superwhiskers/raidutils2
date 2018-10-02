20ebf368c30238321a29505647111f4a0a5c4c5c
superwhiskers <kspy360@rocketmail.com>
2018-10-02 16:46:04 -0500

    fix non-working code and remove binary for linux x64

--- hybrid.go
+++ hybrid.go
@@ -5,6 +5,8 @@ import "fmt"
 func hybridMode() {
 
 	var addr string
+	
+	fmt.Printf("sadily, this does not work yet :p\n")
 
 	if question("are you sure?", []string{"yes", "no"}) == "no" {
 
@@ -17,6 +19,9 @@ func hybridMode() {
 		addr = question("enter the address of the master", []string{})
 
 		// attempt to verify the connection
+		fmt.Printf("address: %s\n", addr)
+
+		return
 
 	}
 
